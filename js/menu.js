211fc86ebccff263e895ff2e468a462511562220
Oleksandr Arseniuk <korol1511@gmail.com>
2022-08-08 08:45:37 +0300

    add js

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,29 @@
-(() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleModal);
-    refs.closeMenuBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.menu.classList.toggle("is-open");
+const menu = document.querySelector(".menu");
+const btn = document.querySelector(".btn");
+// is - hidden
+btn.addEventListener("click", openMenu);
+
+function openMenu() {
+  menu.classList.add("is-open");
+  menu.classList.remove("is-hidden");
+}
+
+document.addEventListener('keydown', evt => {
+    if (evt.key === 'Escape') {
+      menu.classList.remove("is-open");
+      menu.classList.add("is-hidden");
     }
-  })();
+});
+
+document.addEventListener("click", function handleClickOutsideBox(event) {
+  const box = document.getElementById('menu');
+
+  if (!box.contains(event.target)) {
+    console.log("HI")
+  }
+});
+
+  // menu.classList.remove("is-open");
+  // menu.classList.add("is-hidden");
+// const open = menu.classList.add("is-open");
+// console.log(menu);
